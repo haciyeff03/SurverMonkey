28e2df976b9904ca55ab65a4a1cfadb6b1f42589
Qurban Haciyev <haciyevqurban97@gmail.com>
2025-02-04 19:33:26 +0400

    logos adding

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -185,6 +185,21 @@ export default function Signup() {
                         </button>
                     </div>
                 </div>
+
+                <div className={styles.mobile_google}>
+                    <button className={styles.icon}>
+                    <div className={styles.text}>
+                                <Image src="/images/google_logo.svg" alt="Apple" width={26} height={26} />
+                            </div>
+                            <span className={styles.text_icon}>Register via Google</span>
+                    </button>
+                    <button className={styles.icon}>
+                    <div className={styles.text}>
+                                <Image src="/images/apple_logo.svg" alt="Apple" width={26} height={26} />
+                            </div>
+                            <span className={styles.text_icon}>Register via Apple</span>
+                    </button>
+                </div>
             </div>
         </div>
     );
