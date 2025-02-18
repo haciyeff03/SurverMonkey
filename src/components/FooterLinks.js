6e6d01fc226e919dd2351f7a8b3ddfb2474dd657
Qurban Haciyev <haciyevqurban97@gmail.com>
2025-02-19 00:54:10 +0400

    final

--- src/components/FooterLinks.js
+++ src/components/FooterLinks.js
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import styles from './FooterLinks.module.css';
 
+
 export default function FooterLinks() {
     return (
         <div className={styles.footer}>
